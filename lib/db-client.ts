76fdbe872861fdcb334ecd09ace86cf96a3dcc29
Bryan Pan <bryanpan342@gmail.com>
2021-12-02 20:48:18 -0800

    feat: create DbClient to manage database interacitons (#1)
    * Created a class to manage all interactions with the database
    * Updated the OCI instance to have the client library and the wallet
    
    **NOTE**:
    Since our OCI instance is on an ARM processor, there are no prebuild binaries for it. See docs [here](https://oracle.github.io/node-oracledb/INSTALL.html#aarch64). So I have to put in the URL for package manager to build for us. 
    
    TODO:
    - [x] Comment through the code
    - [ ] Find a way to put the oracledb as a fallback for the dependency list

---
+++ lib/db-client.ts
@@ -0,0 +1,102 @@
+import oracledb from 'oracledb';
+import {logger} from '../utils';
+
+/**
+ * A Client for managing database connections.
+ */
+export class DbClient {
+  /**
+   * The Oracle DB connection.
+   *
+   * @throws if the Client is not configured
+   */
+  get connection(): oracledb.Connection {
+    if (this._connection == null) {
+      throw new Error(
+        'DbClient is not connected to Oracle JSON Database. ' +
+        'Make sure you configure the client with DbClient.configure().',
+      );
+    }
+    return this._connection;
+  }
+
+  /**
+   * The soda database connection for Oracle DB.
+   *
+   * @throws if the Client is not configured
+   */
+  get soda(): oracledb.SodaDatabase {
+    if (this._soda == null) {
+      throw new Error(
+        'SODA is not configured on DbClient. ' +
+        'Make sure you configure the client with DbClient.configure().',
+      );
+    }
+    return this._soda;
+  }
+
+  /**
+   * Database collections where the key is the collection name.
+   */
+  public readonly collections: {[name: string]: oracledb.SodaCollection};
+
+  /**
+   * The password for the oracle db connection.
+   */
+  private readonly _password: string;
+  /**
+   * The connection string for the oracle db connection.
+   */
+  private readonly _connectionString: string;
+
+  /**
+   * The private variable containing the connection.
+   */
+  private _connection?: oracledb.Connection;
+  /**
+   * The private variable contianing the soda instance.
+   */
+  private _soda?: oracledb.SodaDatabase;
+
+  public constructor() {
+    oracledb.initOracleClient({configDir: '/home/opc/instantclient_19_10' });
+    this._password = process.env.PASSWORD ?? '';
+    this._connectionString = process.env.CONNECTION_STRING ?? '';
+    this.collections = {};
+  }
+
+  /**
+   * Configures the db client. You must run this function once.
+   */
+  public async configure(): Promise<void> {
+    if (this._connection != null && this._soda != null) return;
+    try {
+      this._connection = await oracledb.getConnection({
+        user: 'admin',
+        password: this._password,
+        connectionString: this._connectionString,
+      });
+      this._soda = await this._connection.getSodaDatabase();
+      logger.info('Oracle DB connection established!');
+    } catch (err) {
+      logger.error(err);
+    }
+  }
+
+  /**
+   * Open a database collection given the collection name.
+   * @param collectionName the name of the collection
+   */
+  public async openCollection(collectionName: string): Promise<oracledb.SodaCollection> {
+    if (this.collections[collectionName] != null)
+      return this.collections[collectionName];
+
+    const collection = await this.soda.openCollection(collectionName);
+    if (collection == undefined) {
+      throw new Error('Collection not found. Make sure you provide the correct collection name.');
+    }
+    this.collections[collectionName] = collection;
+
+    return collection;
+  }
+}
--- www/index.ts
+++ www/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import { DbClient } from '../lib';
 import {logger} from '../utils';
 
+const client = new DbClient();
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Hello!!');
-});
+async function main() {
+  try { await client.configure(); }
+  catch (err) { logger.error(err); }
 
-const PORT = process.env.PORT ?? 3000;
+  app.get('/', async (_req, res) => {
+    const collection = await client.openCollection('test');
+    const count = await collection.find().count();
+    res.send(`Connection to db established!! Count: ${JSON.stringify(count)}.`); });
 
-app.listen(PORT, () => logger.info(`App listening on PORT ${PORT}`));
+  const PORT = process.env.PORT ?? 3000;
+
+  app.listen(PORT, () => logger.info(`App listening on PORT ${PORT}`));
+}
+
+void main();
