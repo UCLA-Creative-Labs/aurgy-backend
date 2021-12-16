5ec0a86bf59cad0472cb1c3a37161a8399274693
Sriram Balachandran <sriramb2000@gmail.com>
2021-12-16 15:31:52 -0800

    fix: stupid linter shit

--- www/lobby/leave.ts
+++ www/lobby/leave.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from 'express';
 
 export async function POST_leave(req: Request, res: Response): Promise<void> {
-  return res.status(200).json({ message: "Filler." });
+  res.status(200).json({ message: "Filler." });
 }
