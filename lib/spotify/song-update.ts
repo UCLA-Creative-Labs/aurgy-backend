3cb1f57bc4014ec250f58dd6cc98938426baca82
Tyler Xu <xutyler937@gmail.com>
2021-12-17 22:15:25 -0800

    linters

--- lib/spotify/song-update.ts
+++ lib/spotify/song-update.ts
@@ -3,35 +3,34 @@ import { HTTPResponseError } from '../../utils/errors';
 import { IArtist, ISong, SongResponse, TopSongResponse } from './song';
 
 export async function getTopSongs(accessToken: string){
-    function getSongs(offset: number){
-        return fetch(`https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term&offset=${offset}`, {
-        method: 'GET',
-        headers: {
+  function getSongs(offset: number){
+    return fetch(`https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term&offset=${offset}`, {
+      method: 'GET',
+      headers: {
         Authorization: `Bearer ${accessToken}`,
-        },
-        });
-    }
-    
-    const responses = await Promise.all([getSongs(0), getSongs(49)])
-    responses.forEach((res: Response) => {
-        if (!res.ok) throw new HTTPResponseError(res);
-    })
+      },
+    });
+  }
 
-    const data = await Promise.all(responses.map(res => res.json()));
+  const responses = await Promise.all([getSongs(0), getSongs(49)]);
+  responses.forEach((res: Response) => {
+    if (!res.ok) throw new HTTPResponseError(res);
+  })
 
-    const songMap = data.reduce((acc: Record<string, ISong>, d: TopSongResponse) => {
-        d.items.forEach((s: SongResponse) => {
-            if (s.id in acc) return;
-            const artistNames = s.artists.reduce((arr: IArtist[], art: IArtist) => {
-                arr.push({'name': art.name, 'id': art.id, 'uri': art.uri})
-                return arr;
-            }, []);
+  const data = await Promise.all(responses.map(res => res.json()));
 
-            acc[s.id] = {'name': s.name,'href': s.href, 'uri': s.uri, 
-                        'popularity': s.popularity, 'duration': s.duration_ms,'artists': artistNames};
-        });
-        return acc;
-    }, {});
+  const songMap = data.reduce((acc: Record<string, ISong>, d: TopSongResponse) => {
+    d.items.forEach((s: SongResponse) => {
+      if (s.id in acc) return;
+      const artistNames = s.artists.reduce((arr: IArtist[], art: IArtist) => {
+        arr.push({name: art.name, id: art.id, uri: art.uri});
+        return arr;
+      }, []);
 
-    return songMap;
+      acc[s.id] = {name: s.name, href: s.href, uri: s.uri, popularity: s.popularity, duration: s.duration_ms, artists: artistNames};
+    });
+    return acc;
+  }, {});
+
+  return songMap;
 }
--- lib/spotify/song.ts
+++ lib/spotify/song.ts
@@ -1,28 +1,28 @@
 export interface IArtist{
-    id: string;
-    uri: string;
-    name: string;
+  id: string;
+  uri: string;
+  name: string;
 }
 
 export interface ISong{
-    name: string;
-    href: string; 
-    uri: string;
-    popularity: number;
-    duration: number;
-    artists: Array<IArtist>;
+  name: string;
+  href: string; 
+  uri: string;
+  popularity: number;
+  duration: number;
+  artists: Array<IArtist>;
 }
 
 export interface SongResponse{
-    id:string;
-    name: string;
-    href: string; 
-    uri: string;
-    popularity: number;
-    duration_ms: number;
-    artists: Array<IArtist>;
+  id:string;
+  name: string;
+  href: string; 
+  uri: string;
+  popularity: number;
+  duration_ms: number;
+  artists: Array<IArtist>;
 }
 
 export interface TopSongResponse{
-    items: SongResponse[];
+  items: SongResponse[];
 }
