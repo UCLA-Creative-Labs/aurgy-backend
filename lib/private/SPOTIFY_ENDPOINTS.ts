fefa334c6df247a8195ea24108c48810333e684f
Bryan Pan <bryanpan342@gmail.com>
2021-12-21 12:45:47 -0800

    chore: refactor top songs implementation to reflect design guidelines

--- lib/private/SPOTIFY_ENDPOINTS.ts
+++ lib/private/SPOTIFY_ENDPOINTS.ts
@@ -1,2 +1,6 @@
+// USER INFO
 export const TOKEN = 'https://accounts.spotify.com/api/token';
 export const ME = 'https://api.spotify.com/v1/me';
+
+// SONGS
+export const TOP_TRACKS = 'https://api.spotify.com/v1/me/top/tracks';
