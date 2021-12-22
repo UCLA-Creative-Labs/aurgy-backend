6d9a874b83982ebac7d2d651bf8243c9ce87a946
Bryan Pan <bryanpan342@gmail.com>
2021-12-22 07:13:18 +0000

    linters

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -4,13 +4,13 @@ import { logger } from '.';
 
 export function genJwt(id: string): string {
   if (!process.env.TOKEN_SECRET) {
-    throw new Error('Error: JWT Token Secret is missing.')
+    throw new Error('Error: JWT Token Secret is missing.');
   }
   return jwt.sign({id}, process.env.TOKEN_SECRET, { expiresIn: '7d' });
 }
 
 export function validateJwt(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
 
   if (token == null) return res.sendStatus(401);
@@ -23,5 +23,5 @@ export function validateJwt(req: Request, res: Response, next: NextFunction) {
 
     req.body.id = decoded.id;
     next();
-  })
+  });
 }
