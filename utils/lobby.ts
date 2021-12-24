78d84f3f284b31b1d9f49c7f9c4898f0fc1c1290
Trevor <tr89on@gmail.com>
2021-12-24 01:09:45 -0800

    update generateLobbyId to use crypto

--- utils/lobby.ts
+++ utils/lobby.ts
@@ -1,25 +1,11 @@
 
-// 16-digit prime
-const HASH_16 = 1000000000100011;
+import { createHash } from 'crypto';
 
-// based off https://stackoverflow.com/questions/6122571/simple-non-secure-hash-function-for-javascript
-const hash16digit = (input : string) : number => {
-  let hash = 0;
-  if (input.length == 0) {
-    return hash;
-  }
-  const chars = [...input];
-
-  /* eslint-disable */
-  chars.forEach((char) => {
-    hash = ((hash << 5) - hash) + char.charCodeAt(0);
-    hash = hash & hash;
-  });
-  /* eslint-enable */
-
-  return Math.abs(hash % HASH_16);
-};
-
-export const generateLobbyId = (userId : string, retryNum : number) : number => {
-  return hash16digit(userId + Date.now() + retryNum);
+export const generateLobbyId = (userId : string, retryNum : number) : string => {
+  // 16-digit prime
+  const hash_16 = 9999999900000001;
+  const input = userId + Date.now() + retryNum;
+  const generated_hash = createHash('sha1').update(input, 'utf-8').digest('hex').toString();
+  const digit_hash = parseInt(generated_hash, 16) % hash_16;
+  return digit_hash.toString();
 };
